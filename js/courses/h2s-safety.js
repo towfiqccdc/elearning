f668f62f48b877d900cb33b7ea9342163703f6cd
towfiqccdc <towfiq.ccdc@gmail.com>
2026-09-12 21:03:46 +0600

    Update h2s-safety.js

--- js/courses/h2s-safety.js
+++ js/courses/h2s-safety.js
@@ -13,6 +13,7 @@ registerCourse({
     category: "H2S",
     level: "Intermediate",
     icon: "bi-cloud-haze2",
+    locked:true,
     summary: "Recognize H₂S hazards, understand exposure risks, and respond safely to an H₂S emergency.",
     description: "This course provides essential knowledge about Hydrogen Sulfide (H₂S), including its properties, health hazards, detection, alarm response, personal protective equipment, emergency procedures, and safe work practices in areas where H₂S may be present.",
     instructor: "CCDC HSE Department",
